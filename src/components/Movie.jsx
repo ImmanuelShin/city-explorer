065368b6483d97cadd2099769f2d00b948257c72
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-07 19:59:50 +0000

    Added additional movie info

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,12 @@
 function Movie(props) {
   return (
-    <div key={props.key}>
-      <p>Description: {props.movie.description}</p>
-      <p>Date: {props.movie.date}</p>
+    <div>
+      <h5>{props.movie.title}</h5>
+      {props.movie.img_url && (
+        <img src={props.movie.img_url} alt={props.movie.title} style={{ width: '50%', maxHeight: '300px', objectFit: 'cover' }} />
+      )}
+      <p>{props.movie.overview}</p>
+      <p>Popularity: {props.movie.pop}</p>
     </div>
   );
 }
